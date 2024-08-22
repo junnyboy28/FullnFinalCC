be46641016319c881e61943c844fe8c81ba879b3
Jimil Mandani <97499168+junnyboy28@users.noreply.github.com>
2024-08-22 22:17:25 +0530

    Update postController.js
    Socket changes

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,3 +1,4 @@
+import { io } from "../socket/socket.js";
 import Post from "../models/postModel.js";
 import User from "../models/userModel.js";
 import { v2 as cloudinary } from "cloudinary";
@@ -32,6 +33,8 @@ const createPost = async (req, res) => {
 
 		const newPost = new Post({ postedBy, text, img });
 		await newPost.save();
+
+		io.emit("newPost", newPost);
 
 		res.status(201).json(newPost);
 	} catch (err) {
@@ -91,16 +94,18 @@ const likeUnlikePost = async (req, res) => {
 
 		const userLikedPost = post.likes.includes(userId);
 
-		if (userLikedPost) {
-			// Unlike post
-			await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
-			res.status(200).json({ message: "Post unliked successfully" });
-		} else {
-			// Like post
-			post.likes.push(userId);
-			await post.save();
-			res.status(200).json({ message: "Post liked successfully" });
-		}
+        if (userLikedPost) {
+            // Unlike post
+            await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
+            io.emit("unlikePost", { postId, userId });
+            res.status(200).json({ message: "Post unliked successfully" });
+        } else {
+            // Like post
+            post.likes.push(userId);
+            await post.save();
+            io.emit("likePost", { postId, userId });
+            res.status(200).json({ message: "Post liked successfully" });
+        }
 	} catch (err) {
 		res.status(500).json({ error: err.message });
 	}
@@ -127,6 +132,8 @@ const replyToPost = async (req, res) => {
 
 		post.replies.push(reply);
 		await post.save();
+
+		io.emit("newComment", { postId, comment: reply });
 
 		res.status(200).json(reply);
 	} catch (err) {
